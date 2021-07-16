69148bc7b2b0c11f0cc2514387d206423f6db053
yuya yamada <yuya19920721@gmail.com>
2021-07-16 16:07:08 +0900

    追加：deviseにおけるストロングパラメータ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  def top
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username image introduction admin])
   end
+
+  def top; end
 end
