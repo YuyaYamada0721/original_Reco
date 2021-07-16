a971ed2b28042a213e916828d9419ef4cd87674a
yuya yamada <yuya19920721@gmail.com>
2021-07-17 01:50:54 +0900

    追加：動作確認のため、Userコントローラとshowアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  root 'application#top'
-
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
+  root 'users#show'
+
+  devise_for :users
+
+  resources :users, only: %i[show edit update]
+
 end
