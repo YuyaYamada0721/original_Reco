08ae7b0b5c76667326b6b6325d30f5e26a90ce5d
yuya yamada <yuya19920721@gmail.com>
2021-07-18 12:11:33 +0900

    修正：チーム名のユニーク制限を解除

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  validates :name, presence: true, length: { in: 1..30 }, uniqueness: true
+  validates :name, presence: true, length: { in: 1..30 }
 
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   belongs_to :user
