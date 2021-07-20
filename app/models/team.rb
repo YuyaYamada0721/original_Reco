5494369963b841e8c9a7cb83c438876afdb5150f
yuya yamada <yuya19920721@gmail.com>
2021-07-20 23:45:21 +0900

    編集：knowledgeテーブルを追加したのでモデルの調整

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,9 @@ class Team < ApplicationRecord
 
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   belongs_to :user
+
   has_many :members, dependent: :destroy
+  has_many :knowledges, dependent: :destroy
 
   def invite_member(user)
     members.create(user: user)
