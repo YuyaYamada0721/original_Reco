6737fa5580174ea6b07d83744f49936eff43207f
yuya yamada <yuya19920721@gmail.com>
2021-07-21 17:55:00 +0900

    更新：invitationメソッドをcontroller/teams/members_controller.rbのcreateへと更新

--- app/controllers/teams/members_controller.rb
+++ app/controllers/teams/members_controller.rb
@@ -1,6 +1,14 @@
 class Teams::MembersController < ApplicationController
-  
-  def create; end
+
+  def create
+    if User.find_by(email: params[:team][:email])
+      @user = User.find_by(email: params[:team][:email])
+      Member.create(user_id: @user.id, team_id: params[:team_id])
+      redirect_to team_path(params[:team_id]), notice: 'メンバーを追加しました。'
+    else
+      redirect_to team_path, notice: '招待できませんでした。'
+    end
+  end
 
   def destroy; end
 end
