aa61e0f4a811ebb5ab0ae641106325afce9086f4
yuya yamada <yuya19920721@gmail.com>
2021-07-21 09:36:03 +0900

    追加：knowledgeを新規登録できるように設定

--- app/controllers/teams/knowledges_controller.rb
+++ app/controllers/teams/knowledges_controller.rb
@@ -4,4 +4,27 @@ class Teams::KnowledgesController < ApplicationController
     @team = Team.find(params[:team_id])
     @knowledges = @team.knowledges.all
   end
+
+  def new
+    @knowledge = Knowledge.new
+  end
+
+  def create
+    @member = Member.where(team_id: params[:team_id]).find_by(user_id: current_user.id)
+    @knowledge = Knowledge.new(member_id: @member.id, team_id: params[:team_id], name: params[:team][:name])
+    if @knowledge.save
+      redirect_to team_knowledges_path, notice: 'ナレッジを登録しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def knowledge_params
+    params.require(:knowledge).permit(:member_id, :team_id, :name)
+  end
+
+    # member = Member.where(team_id: params[:team_id]).where(user_id: current_user.id)
+    # @knowledge = member.knowledges.build
 end
