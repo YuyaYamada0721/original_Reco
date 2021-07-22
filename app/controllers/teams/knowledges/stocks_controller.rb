63bf7436e01f0dfbcd7909d4f7171d521f2649cc
yuya yamada <yuya19920721@gmail.com>
2021-07-22 22:25:25 +0900

    追加：teamのshowページstock解除機能を実施 #11

--- app/controllers/teams/knowledges/stocks_controller.rb
+++ app/controllers/teams/knowledges/stocks_controller.rb
@@ -6,4 +6,9 @@ class Teams::Knowledges::StocksController < ApplicationController
     redirect_to team_knowledge_path(params[:team_id], params[:knowledge_id]), notice: 'ストックしました。'
   end
 
+  def destroy
+    member = Member.find_by(user_id: current_user.id, team_id: params[:team_id])
+    Stock.find_by(member_id: member.id, knowledge_id: params[:knowledge_id]).destroy
+    redirect_to team_knowledge_path(params[:team_id], prams[:knowledge_id]), notice: 'ストックから外しました。'
+  end
 end
