408ae16fdf6ec569e4f04121aa366e7f11acf96b
yuya yamada <yuya19920721@gmail.com>
2021-07-22 19:18:27 +0900

    修正：knowledgeを登録する際のエラー解消

--- app/controllers/teams/knowledges_controller.rb
+++ app/controllers/teams/knowledges_controller.rb
@@ -10,14 +10,16 @@ class Teams::KnowledgesController < ApplicationController
 
   def new
     @knowledge = Knowledge.new
+    @team = Team.find(params[:team_id])
   end
 
   def create
     @member = Member.where(team_id: params[:team_id]).find_by(user_id: current_user.id)
-    @knowledge = Knowledge.new(member_id: @member.id, team_id: params[:team_id], name: params[:team][:name])
+    @knowledge = Knowledge.new(member_id: @member.id, team_id: params[:team_id], name: params[:knowledge][:name])
     if @knowledge.save
       redirect_to team_knowledges_path, notice: 'ナレッジを登録しました。'
     else
+      @team = Team.find(params[:team_id])
       render :new
     end
   end
