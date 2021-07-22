a5139b58164b094743809f215b9ca96f9df794cd
yuya yamada <yuya19920721@gmail.com>
2021-07-22 13:15:25 +0900

    修正：before_action :set_userをそれぞれのアクション内で設定。before_actionの本来の使い方、set_・・・アクションが増えた時に分かりにくい等の理由 #33

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[show edit update]
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
-  def edit; end
+  def edit
+    @user = User.find(params[:id])
+  end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path, notice: '編集しました。'
-      bypass_sign_in(@user) #Userを編集してもログイン状態を維持
+      bypass_sign_in(@user)
     else
       render 'edit'
     end
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :image, :image_cache, :introduction, :admin)
