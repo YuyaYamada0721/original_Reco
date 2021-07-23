7ceabc4592774e8955858a90ac7ec6d969d7a779
yuya yamada <yuya19920721@gmail.com>
2021-07-23 18:24:30 +0900

    更新：tips controller index

--- app/controllers/teams/knowledges/tips_controller.rb
+++ app/controllers/teams/knowledges/tips_controller.rb
@@ -1,3 +1,9 @@
 class Teams::Knowledges::TipsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @team = Team.find(params[:team_id])
+    @tips = @team.tips.all
+  end
+
 end
