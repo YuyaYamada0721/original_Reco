2e91e8dac09095cab8b14977783555a7109428be
yuya yamada <yuya19920721@gmail.com>
2021-07-23 19:09:04 +0900

    更新：tips controller new

--- app/controllers/teams/knowledges/tips_controller.rb
+++ app/controllers/teams/knowledges/tips_controller.rb
@@ -4,7 +4,15 @@ class Teams::Knowledges::TipsController < ApplicationController
   def index
     @team = Team.find(params[:team_id])
     @knowledge = Knowledge.find(params[:knowledge_id])
+    @member = Member.find_by(user_id: current_user.id, team_id: params[:team_id])
     @tips = @team.tips.all
+  end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @knowledge = Knowledge.find(params[:knowledge_id])
+    @member = Member.find_by(user_id: current_user.id, team_id: params[:team_id])
+    @tip = Tip.new(member_id: @member.id, knowledge_id: @knowledge.id, team_id: @team.id)
   end
 
 end
