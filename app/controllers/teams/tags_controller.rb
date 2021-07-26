a8b867220f11fcde3f3feba4cf51de35905b2c1c
yuya yamada <yuya19920721@gmail.com>
2021-07-26 16:39:53 +0900

    追加：tags controller view new 作成 #13

--- app/controllers/teams/tags_controller.rb
+++ app/controllers/teams/tags_controller.rb
@@ -3,4 +3,15 @@ class Teams::TagsController < ApplicationController
     @team = Team.find(params[:team_id])
     @tags = @team.tags.all
   end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @tag = Tag.new
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:team_id, :name)
+  end
 end
