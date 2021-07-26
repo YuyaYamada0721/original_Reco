f3f038259939a8ad887e7fa73d1068ea09caa440
yuya yamada <yuya19920721@gmail.com>
2021-07-26 16:47:33 +0900

    更新：tags controller create #13

--- app/controllers/teams/tags_controller.rb
+++ app/controllers/teams/tags_controller.rb
@@ -9,9 +9,18 @@ class Teams::TagsController < ApplicationController
     @tag = Tag.new
   end
 
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to team_tags_path, notice: 'タグを登録しました。'
+    else
+      render :new
+    end
+  end
+
   private
 
   def tag_params
-    params.require(:tag).permit(:team_id, :name)
+    params.require(:tag).permit(:name).merge(team_id: params[:team_id].to_i)
   end
 end
