9f86501983c2d75146a09d2e4c65c2d2b8d71237
yuya yamada <yuya19920721@gmail.com>
2021-07-31 13:51:27 +0900

    更新：group controller 調整 #56

--- app/controllers/teams/groups_controller.rb
+++ app/controllers/teams/groups_controller.rb
@@ -1,7 +1,8 @@
 class Teams::GroupsController < ApplicationController
 
   def create
-    @current_member = Member.find_by(team_id: params[:team_id], user_id: current_user.id)
+    @team = Team.find(params[:team_id])
+    @current_member = Member.find_by(team_id: @team.id, user_id: current_user.id)
     @group = Group.create(is_dm: true)
     @group_member1 = GroupMember.create(group_id: @group.id, member_id: @current_member.id)
     @group_member2 = GroupMember.create(params.require(:group_member).permit(:member_id, :group_id).merge(group_id: @group.id))
@@ -9,14 +10,16 @@ class Teams::GroupsController < ApplicationController
   end
 
   def show
-    @current_member = Member.find_by(team_id: params[:team_id], user_id: current_user.id)
+    @team = Team.find(params[:team_id])
+    @current_member = Member.find_by(team_id: @team.id, user_id: current_user.id)
     @group = Group.find(params[:id])
+
     if GroupMember.where(member_id: @current_member.id, group_id: @group.id).present?
       @messages = @group.messages
       @message = Message.new
       @group_members = @group.group_members
     else
-      credirect_back(fallback_location: root_path)
+      redirect_to team_path(@team), notice: 'グループを作成して下さい。'
     end
   end
 end
