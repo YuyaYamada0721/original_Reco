9b6575d31376e77b569996a5cc2d7635a9155020
yuya yamada <yuya19920721@gmail.com>
2021-07-31 13:53:55 +0900

    更新：teams/messages controller 調整 #56

--- app/controllers/teams/messages_controller.rb
+++ app/controllers/teams/messages_controller.rb
@@ -6,7 +6,7 @@ class Teams::MessagesController < ApplicationController
       @message = Message.create(params.require(:message).permit(:member_id, :body, :group_id).merge(member_id: @current_member.id))
       redirect_to team_group_path(params[:team_id], @message.group_id)
     else
-      redirect_back(fallback_location: root_path)
+      redirect_to team_path(params[:team_id]), notice: 'メッセージが作成できませんでした。'
     end
   end
 
