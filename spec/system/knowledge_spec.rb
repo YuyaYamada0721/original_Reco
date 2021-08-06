e9bf69416e384ec173c9f22a66b23302decd1641
yuya yamada <yuya19920721@gmail.com>
2021-08-06 21:17:54 +0900

    更新：knowledge System Spec #89

---
+++ spec/system/knowledge_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+RSpec.describe 'ナレッジ機能', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @user2 = FactoryBot.create(:user2)
+    @team = FactoryBot.create(:team, user_id: @user.id, owner_id: @user.id)
+    @member = FactoryBot.create(:member, user_id: @user.id, team_id: @team.id)
+    @group = FactoryBot.create(:group)
+
+    FactoryBot.create(:team, user: @user, owner: @user)
+    FactoryBot.create(:group_member, member: @member, group: @group)
+    FactoryBot.create(:knowledge, member: @member, team: @team)
+    FactoryBot.create(:knowledge2, member: @member, team: @team)
+
+    visit new_user_session_path
+    fill_in 'user[email]', with: 'piyo@piyo.com'
+    fill_in 'user[password]', with: 'piyopiyo'
+    find('.devise-btn').click
+    click_on '個人記録・チーム記録へ'
+    click_on 'テストチーム'
+    click_on 'ナレッジ'
+  end
+
+  describe 'ナレッジ登録機能' do
+    context 'ナレッジ登録操作をした場合' do
+      it 'ナレッジ登録が完了し、ナレッジ一覧画面へ遷移される' do
+        click_on 'ナレッジ登録'
+        fill_in 'knowledge[name]', with: 'テストナレッジ'
+        click_on '登録する'
+        expect(page).to have_content 'テストナレッジ'
+        expect(page).to have_content 'ナレッジを登録しました。'
+      end
+    end
+  end
+  describe 'ナレッジ詳細機能' do
+    context 'ナレッジ詳細操作をした場合' do
+      it 'ナレッジの詳細画面へ遷移される' do
+        click_on 'テストナレッジ'
+        expect(page).to have_content 'テストナレッジ'
+      end
+    end
+  end
+  describe 'ナレッジ編集機能' do
+    context 'ナレッジ編集操作をした場合' do
+      it 'ナレッジ編集ができる' do
+        click_on 'テストナレッジ'
+        click_on 'ナレッジ編集'
+        fill_in 'knowledge[name]', with: 'ナレッジ編集機能テスト'
+        click_on '登録する'
+        expect(page).to have_content 'ナレッジ編集機能テスト'
+        expect(page).to have_content 'ナレッジを編集しました。'
+      end
+    end
+  end
+  describe 'ナレッジ削除機能' do
+    context 'ナレッジ削除操作をした場合' do
+      it 'ナレッジ削除ができる' do
+        click_on 'テストナレッジ'
+        click_on 'ナレッジ編集'
+        click_on '削除'
+        page.accept_alert
+        expect(page).not_to have_content 'テストナレッジ'
+        expect(page).to have_content 'ナレッジを削除しました。'
+      end
+    end
+  end
+  describe 'ナレッジ検索機能' do
+    context 'ナレッジの検索操作をした場合' do
+      it '検索したナレッジが表示される' do
+        fill_in 'q[name_cont]', with: 'fugaナレッジ'
+        click_on '検索'
+        expect(page).to have_content 'fugaナレッジ'
+        expect(page).not_to have_content 'テストナレッジ'
+      end
+    end
+  end
+end
