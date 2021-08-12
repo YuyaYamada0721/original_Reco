5ff060323790fde453e019e1027e2a579c646914
yuya yamada <yuya19920721@gmail.com>
2021-08-12 20:02:33 +0900

    追加：helpページ外側 #111

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root 'tops#index'
 
+  get 'help', action: :show, controller: 'helps'
+
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
