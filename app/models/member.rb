8c50150952ca5535e8154a06df059b7a1ea70d8c
yuya yamada <yuya19920721@gmail.com>
2021-09-04 12:51:16 +0900

    更新：team_controller リファクタ #183

--- app/models/member.rb
+++ app/models/member.rb
@@ -15,6 +15,8 @@ class Member < ApplicationRecord
 
   before_create :solo_team?
 
+  scope :random_one, ->(members) { offset(rand(members.count)).first }
+
   def solo_team?
     @team = Team.find(team_id)
     throw(:abort) if @team.is_solo == true && @team.members.count == 1
