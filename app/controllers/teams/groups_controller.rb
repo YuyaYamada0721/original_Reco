13762c569ff5a1319a9c2531898c8daf1b34438c
yuya yamada <yuya19920721@gmail.com>
2021-09-05 21:37:07 +0900

    修正：既読機能が正常に動作していなかったため調整 #185

--- app/controllers/teams/groups_controller.rb
+++ app/controllers/teams/groups_controller.rb
@@ -14,19 +14,17 @@ class Teams::GroupsController < ApplicationController
     @team = Team.find(params[:team_id])
     @current_member = Member.find_by(team_id: @team.id, user_id: current_user.id)
     @group = Group.find(params[:id])
+    @message = Message.new
+    @messages = @group.messages.order(id: 'DESC')
+    @group_members = @group.group_members.page(params[:page]).per(10)
 
-    if GroupMember.where(member_id: @current_member.id, group_id: @group.id).present?
-      @messages = @group.messages.order(id: 'DESC')
-      @message = Message.new
-      @group_members = @group.group_members.page(params[:page]).per(10)
+    if Message.joins(:reads).find_by(reads: { is_checked: false }, reads: { member_id: @current_member.id }, group_id: @group.id).present?
 
-      @reads = Read.where(member_id: @current_member.id)
-        @reads.each do |read|
-          read.update(is_checked: true)
-        end
-# チャット画面を開いた時点で、Readテーブルにある自分のメンバーidにヒットするレコード(メッセージ)に既読としてis_checkedにtrueを入れる
-    else
-      redirect_to team_path(@team), notice: 'アクセスできません'
+      @unread_messages = Message.joins(:reads).where(reads: { is_checked: false }, reads: { member_id: @current_member.id }, group_id: @group.id)
+      @unread_messages.each do |message|
+        read = Read.find_by(member_id: @current_member.id, message_id: message.id)
+        read.update(is_checked: true)
+      end
     end
   end
 end
