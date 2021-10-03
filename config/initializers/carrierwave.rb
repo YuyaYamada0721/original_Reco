d244a1db7713aa9c359f31059eacd6e45b9a3bd6
yuya yamada <yuya19920721@gmail.com>
2021-10-03 12:30:07 +0900

    ローカルで画像が反映されるように設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,20 +5,20 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                'ap-northeast-1',
-      path_style:            true,
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1',
+    path_style: true
   }
   config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.fog_attributes = { 'Cache-Control' => 'public, max-age=86400' }
   case Rails.env
-    when 'production'
-      config.fog_directory = 'recoreco-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recoreco-production'
-    when 'development'
-      config.fog_directory = 'recoreco-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recoreco-development'
+  when 'production'
+    config.fog_directory = 'recoreco-production'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recoreco-production'
+  when 'development'
+    config.fog_directory = 'recoreco-development'
+    config.asset_host = 'http://localhost:3000'
   end
 end
